d35d19942175657c1cf4067e155c2b69829863ef
Benefrancis do Nascimento <benefrancis@gmail.com>
2022-05-26 13:45:16 -0300

    Criando os dois projetos Spring

---
+++ aws_project01_start/src/main/java/br/com/benefrancis/aws_project01_start/AwsProject01StartApplication.java
@@ -0,0 +1,13 @@
+package br.com.benefrancis.aws_project01_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProject01StartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProject01StartApplication.class, args);
+	}
+
+}
---
+++ aws_project01_start/src/test/java/br/com/benefrancis/aws_project01_start/AwsProject01StartApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.benefrancis.aws_project01_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProject01StartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
