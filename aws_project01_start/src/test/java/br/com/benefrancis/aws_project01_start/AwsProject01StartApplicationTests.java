a390ab913c612cf7be64fd74f2792b0085a086f5
Benefrancis do Nascimento <benefrancis@gmail.com>
2022-05-26 19:46:17 -0300

    grandle project

--- aws_project01_start/src/test/java/br/com/benefrancis/aws_project01_start/AwsProject01StartApplicationTests.java
+++ aws_project01_start/src/test/java/br/com/benefrancis/aws_project01_start/AwsProject01StartApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AwsProject01StartApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
